e80b3d7206821a6b8aa0205a33546eea7d044243
Nishant Chorge <nishantchorge79@gmail.com>
2022-03-15 15:24:28 +0530

    fixed theme toggle wrong label and state

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -6,29 +6,66 @@ import Contact from './Contact';
 import Project from './Project';
 import NotFound from './NotFound';
 import Theme from './Theme';
-import { useEffect, useRef } from 'react';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { useEffect, useRef, useState } from 'react';
 import Particle from './Particle';
 
 const App = (): JSX.Element => {
 
+  const [isDarkTheme, setDarkTheme] = useState(false);
+
   useEffect((): void => {
-    console.info(`Made with ❤ by Nishant Chorge in React.JS`)
+    // console.info(`Made with ❤ by Nishant Chorge in React.JS`)
 
-    !localStorage.getItem("theme") && localStorage.setItem("theme", "dark");
-    localStorage.getItem("theme") === "dark" ? document.documentElement.setAttribute("data-theme", "dark") : document.documentElement.setAttribute("data-theme", "");
+    let theme = localStorage.getItem("theme");
 
-  }, [])
+    if (!theme || !theme.match(/^(dark|light)$/)) {
+      if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        localStorage.setItem("theme", "dark");
+        document.documentElement.setAttribute("data-theme", "dark")
+        setDarkTheme(p => true);
+      } else {
+        localStorage.setItem("theme", "light")
+        document.documentElement.setAttribute("data-theme", "");
+        setDarkTheme(p=> false)
+      }
+    } else {
+      if (theme === "dark") {
+        document.documentElement.setAttribute("data-theme", "dark")
+        setDarkTheme(p => true);
+      } else {
+        document.documentElement.setAttribute("data-theme", "");
+        setDarkTheme(p=> false)
+      }
+    }
+  }, [isDarkTheme])
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const isMobile = useRef(window.innerWidth > 700 ? false : true)
+  const isMobile = useRef(window.innerWidth > 700 ? false : true);
+  const container = useRef<HTMLDivElement>(null);
 
-  const isDark = localStorage.getItem("theme") === "dark" ? true : false;
+  const updateTheme = (): void => {
+    let root = document.documentElement;
+
+    if (isDarkTheme) {
+      root.removeAttribute("data-theme");
+      localStorage.setItem("theme", "light")
+      setDarkTheme((): false => false);
+    } else {
+      root.setAttribute("data-theme", "dark");
+      localStorage.setItem("theme", "dark")
+      setDarkTheme((): true => true);
+    }
+  };
+
+  const detectGesture = (event: React.TouchEvent<HTMLDivElement>) => {
+  
+    // !(event.changedTouches[0].clientX < (window.innerWidth / 2))
+
+  }
 
   return (
     <div className="main">
       <Sidebar />
-        <div className="container">
+      <div className="container" ref={container} onTouchEnd={(event)=> isMobile.current ? detectGesture(event): console.log()}>
           <Switch>
             <Route exact path="/" component={About} />
             <Route exact path="/project" component={Project} />
@@ -36,7 +73,7 @@ const App = (): JSX.Element => {
             <Route path="/about" component={About} />
             <Route component={NotFound} />
           </Switch>
-          <Theme isDarkTheme={isDark} />
+        <Theme isDarkTheme={isDarkTheme} updateTheme={updateTheme}/>
         </div>
       {!isMobile.current && <Particle />}
     </div>
--- client/src/components/Theme.tsx
+++ client/src/components/Theme.tsx
@@ -1,36 +1,22 @@
-import { useState } from "react";
 import "../styles/theme.scss";
 
-function Theme({ isDarkTheme }: any): JSX.Element {
+interface Props{
+  isDarkTheme: boolean,
+  updateTheme: () => void
+}
 
-  const [isDark, setTheme] = useState((): boolean => isDarkTheme);
-
-  const updateTheme = (): void => {
-
-    let root = document.documentElement;
-
-    if (isDark) {
-      root.removeAttribute("data-theme");
-      localStorage.setItem("theme", "light")
-      setTheme((): false => false);
-    } else {
-      root.setAttribute("data-theme", "dark");
-      localStorage.setItem("theme", "dark")
-      setTheme((): true => true);
-    }
-  };
-
-
+const Theme = (props: Props): JSX.Element => {
   return (
-    <div className="theme_toggle" aria-checked={isDark}>
-      <input type="checkbox" id="theme_checkbox" className="theme_checkbox" />
-      <label htmlFor="theme_checkbox" className={`toggle ${isDark ? "dark" : ""}`} onClick={updateTheme} title={`Switch to ${isDark ? "light" : "dark"} mode`}>
-        <h6 className="theme_title">go {isDark ? "Light" : "Dark"}</h6>
-        <span className={`toggle_handler ${isDark ? "theme_change" : ""}`}>
+    <div className="theme_toggle" aria-checked={props.isDarkTheme}>
+      <input type="checkbox" id="theme_checkbox" />
+      <label htmlFor="theme_checkbox" className={`toggle ${props.isDarkTheme ? "dark" : ""}`} onClick={()=>props.updateTheme()} title={`Switch to ${props.isDarkTheme ? "light" : "dark"} mode`}>
+        <h6 className="theme_title">go {props.isDarkTheme ? "Light" : "Dark"}</h6>
+        
+        <span className={`toggle_handler ${props.isDarkTheme ? "theme_change" : ""}`}>
         </span>
       </label>
     </div>
   );
 }
 
-export default Theme
+export default Theme;
